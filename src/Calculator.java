fb293b710eed1b72a3f30d948e64696df712d86d
sridhar16pdy@gmail.com <sridharravindar@gmail.com>
2025-11-09 22:29:00 +0530

    C2: Added add() in Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,8 @@ class Calculator {
     public static void main(String[] args) {
         System.out.println("Calculator");
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
